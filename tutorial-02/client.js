f899b221e4e588c8dab5cc38025e0baa987a078e
Mike Amundsen <mamund@yahoo.com>
2013-11-03 13:00:45 -0500

    add Handling a Cj Response to the tutorial

--- tutorial-02/client.js
+++ tutorial-02/client.js
@@ -35,6 +35,34 @@ cjClient = function() {
     }
   }  
   
+  function handleCjResponse() {
+    var div, links, i, x, ul, li, a;
+
+    // show version and rootUrl
+    div = document.getElementById('rawcj');
+    if(div) {
+      div.innerHTML = '';
+      div.innerHTML = 'Version: ' + getVersion(g.data) + '<br />';
+      div.innerHTML += 'RootURL: ' + getRootUrl(g.data);
+    }
+
+    // add any top-level links
+    if(g.data.collection.links) {
+      links = g.data.collection.links;
+
+      ul = document.createElement('ul');
+      ul.className = 'topLinks';
+
+      for(i=0,x=links.length;i<x;i++) {
+        a = getLink(links[i]);
+        li = document.createElement('li');
+        li.appendChild(a);
+        ul.appendChild(li);
+      }
+      div.appendChild(ul);
+    }
+  }
+
   function getResponse(href) {
     var ajax;
 
@@ -51,6 +79,7 @@ cjClient = function() {
       if(ajax.status===200 || ajax.status===204) {
         g.data = JSON.parse(ajax.responseText);
         displayRawCj();
+        //handleCjResponse();
       }
       else {
         alert('*** ERROR: '+ajax.status+'\n'+ajax.statusText);
@@ -58,6 +87,34 @@ cjClient = function() {
     }
   }
   
+  function getVersion(data) {
+    return (data.collection.version?data.collection.version:'0.0');
+  }
+
+  function getRootUrl(data) {
+    return (data.collection.href?data.collection.href:'');  
+  }
+
+  function getLink(link) {
+    var rtn;
+
+    if(link.render && link.render==='image') {
+      rtn = document.createElement('img');
+      rtn.src = (link.href?link.href:'#');
+      rtn.className = (link.rel?link.rel:'');
+      rtn.alt = (link.prompt?link.prompt:rtn.src);
+      rtn.title = rtn.alt;
+    }
+    else {
+      rtn = document.createElement('a');
+      rtn.href = (link.href?link.href:'#');
+      rtn.rel = (link.rel?link.rel:'');
+      rtn.appendChild(document.createTextNode((link.prompt?link.prompt:rtn.rel)));
+      rtn.title = rtn.rel;
+    }
+    return rtn;  
+  }
+
   // register events and return
   var that = {};
   that.init = init;
